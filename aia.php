da152aa884656ab39575f51211b76e893960d1b4
John Laveta <lavetajm@gmail.com>
2019-10-03 14:00:06 -0700

    Main site initial commit

---
+++ aia.php
@@ -0,0 +1,23 @@
+***REMOVED***
+<html>
+<head>
+<title>AIA Page</title>
+</head>
+
+<body>
+<!-- START WRAPPER -->
+<main class="wrapper">
+<header>
+  <h1><a href="index.php"><i class="logo fa fa-home"></i> AIA Page</a></h1>
+</header>
+ 
+<!-- START Footer -->
+<footer>
+  <p><small>&copy; 2018 - <?=date('Y')***REMOVED*** by <a href="contactme.php" target="_blank">John Laveta</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+</footer>
+<!-- END Footer --> 
+</main>
+<!-- END WRAPPER -->
+
+</body>
+***REMOVED***

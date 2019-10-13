8253c2b6e53148f30ae6c8cc9ee39448018c71b9
John Laveta <lavetajm@gmail.com>
2019-10-12 21:51:45 -0700

    Moved header and footer to include files.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 ***REMOVED***
-<html>
+***REMOVED***
 <head>
 <title>John Laveta</title>
 <meta name="viewport" content="width=device-width" />
@@ -14,20 +14,7 @@
 <body>
 <!-- START WRAPPER -->
 <main class="wrapper">
-<header>
-  <h1><a href="index.php"><i class="logo fa fa-home"></i> John Laveta's SCC WEB120 Portal</a></h1>
-  <nav>
-    <ul class="topnav" id="myTopnav">
-      <li><a href="index.php" class="selected">Welcome</a></li>
-      <li><a href="big/index.php">Big</a></li>
-      <li><a href="aia.php">AIA</a></li>
-      <li><a href="flowchart.php">Flowchart/Layout</a></li>
-      <li><a href="fp/index.php">Final Project</a></li>
-      <li><a href="contactme.php">Contact Sara</a></li>
-      <li class="icon"> <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a> </li>
-    </ul>
-  </nav>
-</header>
+***REMOVED*** include './includes/header';***REMOVED***
         
 <!-- START LEFT COL -->
 <section>
@@ -36,8 +23,8 @@
 <!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
  <img src="images/beach.jpg" class="desktop" alt="" />
  <img src="images/mini_golf.jpg" class="phone" alt="" />
- <p>Application developer with a history in medical service engineering 
- and aeronautical electronics repair. Adept at project management 
+ <p>Greetings! I am an application developer with a history in medical service 
+ engineering and aeronautical electronics repair. Adept at project management 
  and developing web and mobile applications in a number of different 
  environments. Offering an aptitude for troubleshooting under pressure, 
  the ability to rapidly learn new skills, and the flexibility to adapt 
@@ -56,15 +43,13 @@
 <aside>
  <h3>Right Column</h3>
  <img src="images/main.jpg" class="tablet" alt="" />
- <p>Lorem Ipsum place holder text goes here for now</p>
- <p>Lorem Ipsum place holder text goes here for now</p>
+ <p>I've completed several web application projects in the past, but now hope to further
+ refine those skills.</p>
 </aside>
 <!-- END RIGHT COL -->
  
 <!-- START Footer -->
-<footer>
-  <p><small>&copy; 2018 - <?=date('Y')***REMOVED*** by <a href="contactme.php" target="_blank">Put your name here - not Sara's you will loose points if it's Sara!</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
-</footer>
+***REMOVED*** include './includes/footer.php';***REMOVED***
 <!-- END Footer --> 
 </main>
 <!-- END WRAPPER -->
